7f89f30fd814295a9cc0277c36799d6747c83fd5
mandihamza <mandihamza@gmail.com>
2022-02-17 17:13:30 -0700

    add donation button to nav

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Footer from '@components/Footer'
 import Navigation from '@components/Navigation'
+import Script from 'next/script'
 
 export default function Home() {
   return (
     <div>
-      
-
+       <Script type="text/javascript" defer src="https://donorbox.org/install-popup-button.js" />
+      <Script defer
+        onLoad={() => {window.DonorBox = { widgetLinkClassName: 'custom-dbox-popup' }}}/>
       <main>
         <Navigation />
       </main>
