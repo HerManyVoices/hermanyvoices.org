37d3e6d76a7f60aaca15886353bb45dfd50bd74c
mandihamza <mandihamza@gmail.com>
2022-02-28 18:11:53 -0700

    add components for home page

---
+++ components/NewsletterCTA.js
@@ -0,0 +1,79 @@
+import Link from 'next/link'
+
+export default function NewsletterCTA() {
+  return (
+    <section className="bg-green-600 py-20">
+      <div className="container mx-auto px-4">
+        <div className="-mx-4 flex flex-wrap items-center justify-center">
+          <div className="mb-16 max-w-2xl px-4 text-center lg:mb-0 lg:w-1/2">
+            <div
+              className=" mb-6 inline-block text-3xl font-bold leading-none text-white"
+            >
+
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-16 w-16"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+                />
+              </svg>
+            </div>
+            <h2 className="mb-4 text-center font-heading text-4xl font-bold text-white md:text-5xl">
+              How you can help
+            </h2>
+            <p className="mb-8 text-center leading-loose text-gray-50">
+              Get involved. Small acts of kindness have big impact.
+            </p>
+            <Link href="/support-us">
+              <a className="inline-block rounded-l-xl rounded-t-xl bg-white py-2 px-6 font-bold leading-loose text-green-600 transition duration-200 hover:bg-gray-50">
+                Support Us
+              </a>
+            </Link>
+          </div>
+          <div className="w-full px-4 lg:w-1/2">
+            <div className="mx-auto max-w-sm lg:mr-0 lg:ml-auto">
+              <div className="mb-6 rounded-t-3xl rounded-bl-3xl bg-white py-8 px-6 text-center shadow">
+                <form action="">
+                  <div className="mb-6">
+                    <h4 className="text-2xl">Sign up for our newsletter</h4>
+                  </div>
+                  <div className="-mx-2 mb-4 flex flex-wrap">
+                    <div className="mb-4 w-full px-2 lg:mb-0 lg:w-1/2">
+                      <input
+                        className="w-full rounded border bg-white py-2 px-3 leading-loose"
+                        type="text"
+                        placeholder="First Name"
+                      ></input>
+                    </div>
+                    <div className="w-full px-2 lg:w-1/2">
+                      <input
+                        className="w-full rounded border bg-white py-2 px-3 leading-loose"
+                        type="text"
+                        placeholder="Last Name"
+                      ></input>
+                    </div>
+                  </div>
+                  <input
+                    className="mb-4 w-full rounded border bg-white py-2 px-3 leading-loose"
+                    type="email"
+                    placeholder="hello@example.com"
+                  ></input>
+                  <button className="mb-4 w-full rounded bg-green-600 py-4 text-sm font-bold leading-normal text-white transition duration-200 hover:bg-green-700">
+                    Sign Up
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
---
+++ components/Projects.js
@@ -0,0 +1,76 @@
+export default function Projects() {
+    return (
+        <section>
+  <div className="skew skew-top mr-for-radius">
+    <svg className="h-8 md:h-12 lg:h-20 w-full text-green-600" viewBox="0 0 10 10" preserveAspectRatio="none">
+      <polygon fill="currentColor" points="0 0 10 10 0 10"></polygon>
+    </svg>
+  </div>
+  <div className="skew skew-top ml-for-radius">
+    <svg className="h-8 md:h-12 lg:h-20 w-full text-green-600" viewBox="0 0 10 10" preserveAspectRatio="none">
+      <polygon fill="currentColor" points="0 10 10 0 10 10"></polygon>
+    </svg>
+  </div>
+  <div className="py-20 bg-green-600 radius-for-skewed">
+    <div className="container px-4 mx-auto">
+      <div className="mb-16 flex flex-wrap justify-center md:justify-between items-center">
+        <div className="text-center lg:text-left">
+          <span className="text-green-400 font-bold">Dolor sit amet consectutar</span>
+          <h2 className="text-4xl lg:text-5xl font-bold font-heading text-white">Featured Projects</h2>
+        </div>
+        <a className="hidden md:inline-block py-2 px-6 rounded-l-xl rounded-t-xl bg-white hover:bg-gray-50 font-bold leading-loose transition duration-200" href="#">View More Projects</a>
+      </div>
+      <div className="flex flex-wrap -mx-4 mb-4">
+        <div className="w-full md:w-1/2 lg:w-1/3 mb-8 px-4">
+          <a href="#">
+            <img className="h-80 w-full mx-auto object-cover rounded" src="https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?ixlib=rb-1.2.1&amp;ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;auto=format&amp;fit=crop&amp;w=1050&amp;q=80" alt=""></img>
+          </a>
+        </div>
+        <div className="relative mb-4 w-full md:w-1/2 lg:w-1/3 px-4">
+          <div className="relative h-80 mb-5 mx-auto rounded-lg">
+            <img className="w-full relative h-full rounded-lg object-cover" src="https://images.unsplash.com/photo-1454496522488-7a8e488e8606?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1055&amp;q=80" alt=""></img>
+            <div className="absolute inset-0 bg-gray-900 opacity-75 rounded-lg"></div>
+            <div className="absolute inset-0 p-6 flex flex-col items-start">
+              <span className="text-gray-400">2021</span>
+              <p className="mb-auto text-xl lg:text-2xl text-white font-bold">Lorem ipsum dolor sit amet consectutar</p>
+              <a className="inline-block py-2 px-4 border-2 border-gray-400 hover:border-white bg-transparent text-gray-50 hover:bg-white hover:text-gray-900 transition duration-200 rounded-l-xl rounded-t-xl font-bold leading-loose" href="#">View Project</a>
+            </div>
+          </div>
+        </div>
+        <div className="w-full md:w-1/2 lg:w-1/3 mb-8 px-4">
+          <a href="#">
+            <img className="h-80 w-full mx-auto object-cover rounded" src="https://images.unsplash.com/photo-1518098268026-4e89f1a2cd8e?ixlib=rb-1.2.1&amp;ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;auto=format&amp;fit=crop&amp;w=968&amp;q=80" alt=""></img>
+          </a>
+        </div>
+        <div className="w-full md:w-1/2 lg:w-1/3 mb-8 px-4">
+          <a href="#">
+            <img className="h-80 w-full mx-auto object-cover rounded" src="https://images.unsplash.com/photo-1476610182048-b716b8518aae?ixlib=rb-1.2.1&amp;ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;auto=format&amp;fit=crop&amp;w=1127&amp;q=80" alt=""></img>
+          </a>
+        </div>
+        <div className="w-full md:w-1/2 lg:w-1/3 mb-8 px-4">
+          <a href="#">
+            <img className="h-80 w-full mx-auto object-cover rounded" src="https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixlib=rb-1.2.1&amp;ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;auto=format&amp;fit=crop&amp;w=1050&amp;q=80" alt=""></img>
+          </a>
+        </div>
+        <div className="w-full md:w-1/2 lg:w-1/3 mb-8 px-4">
+          <a href="#">
+            <img className="h-80 w-full mx-auto object-cover rounded" src="https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?ixlib=rb-1.2.1&amp;ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;auto=format&amp;fit=crop&amp;w=1050&amp;q=80" alt=""></img>
+          </a>
+        </div>
+      </div>
+      <div className="text-center"><a className="md:hidden inline-block py-2 px-6 rounded-l-xl rounded-t-xl bg-green-600 hover:bg-green-700 text-gray-50 font-bold leading-loose transition duration-200" href="#">View More Projects</a></div>
+    </div>
+  </div>
+  <div className="skew skew-bottom mr-for-radius">
+    <svg className="h-8 md:h-12 lg:h-20 w-full text-green-600" viewBox="0 0 10 10" preserveAspectRatio="none">
+      <polygon fill="currentColor" points="0 0 10 0 0 10"></polygon>
+    </svg>
+  </div>
+  <div className="skew skew-bottom ml-for-radius">
+    <svg className="h-8 md:h-12 lg:h-20 w-full text-green-600" viewBox="0 0 10 10" preserveAspectRatio="none">
+      <polygon fill="currentColor" points="0 0 10 0 10 10"></polygon>
+    </svg>
+  </div>
+</section>
+    )
+}
