036d3d9211cfa0127d710d424e857191a6bab89e
mandihamza <mandihamza@gmail.com>
2022-03-10 11:05:13 -0700

    add security headers

--- next.config.js
+++ next.config.js
@@ -1,17 +1,58 @@
-/** @type {import('next').NextConfig} */
+/** @type
+ * {import('next').NextConfig}
+ * */
+
+const securityHeaders = [
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload',
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN',
+  },
+  {
+    key: 'Permissions-Policy',
+    value: 'camera=(), microphone=(), geolocation=()',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'strict-origin-when-cross-origin',
+  },
+]
+
 module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ]
+  },
   reactStrictMode: true,
   images: {
     domains: ['images.unsplash.com', 'res.cloudinary.com'],
     formats: ['image/webp'],
-    // path: `${basePath}/_next/image`,
   },
   presets: ['next/babel'],
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
-      use: [{loader: '@svgr/webpack', options: {icon: true}}],
+      use: [{ loader: '@svgr/webpack', options: { icon: true } }],
     })
     return config
-  }
+  },
 }
