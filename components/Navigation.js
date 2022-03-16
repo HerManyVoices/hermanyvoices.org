e821c4974b08abe89799fc2175dd9437217c9f64
mandihamza <mandihamza@gmail.com>
2022-03-15 23:03:32 -0600

    update links and buttons

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,10 +1,10 @@
 import Image from 'next/image'
-import React, { useState } from 'react'
-import logo from 'public/assets/logo/logo.png'
-import FacebookSocialLogo from 'public/assets/social/FacebookSocialLogo.js'
-import TwitterSocialLogo from 'public/assets/social/TwitterSocialLogo.js'
-// import InstagramSocialLogo from '/public/assets/social/InstagramSocialLogo.js'
 import Link from 'next/link'
+import FacebookSocialLogo from 'public/assets/social/FacebookSocialLogo.js'
+import InstagramSocialLogo from 'public/assets/social/InstagramSocialLogo.js'
+import TwitterSocialLogo from 'public/assets/social/TwitterSocialLogo.js'
+import wordmark from 'public/assets/logo/logo-wordmark.png'
+import logo from 'public/assets/logo/logo.png'
 
 export default function Navigation() {
   const [isOpen, setisOpen] = useState(false)
@@ -14,11 +14,30 @@ export default function Navigation() {
   }
 
   return (
-    <div>
-      <nav className="relative flex items-center justify-between bg-gray-50 px-6 py-6">
-        <div className="text-3xl font-bold leading-none">
+    <div className="sticky top-0 z-30">
+      <nav className="relative flex items-center justify-between bg-gray-50 px-6 py-2">
+        <div className=" text-xs font-bold leading-none">
           <Link href="/" passHref>
-            <Image src={logo} className="h-12" alt="" width="44" height="52" />
+            <a>
+              <div className="md:hidden">
+                <Image
+                  src={logo}
+                  className="h-12"
+                  alt=""
+                  width="50"
+                  height="50"
+                />
+              </div>
+              <div className="hidden md:block">
+                <Image
+                  src={wordmark}
+                  className="h-12"
+                  alt=""
+                  width="250"
+                  height="59"
+                />
+              </div>
+            </a>
           </Link>
         </div>
         <div className="lg:hidden">
@@ -38,7 +57,7 @@ export default function Navigation() {
         </div>
         <ul className="absolute top-1/2 left-1/2 hidden -translate-y-1/2 -translate-x-1/2 transform lg:mx-auto lg:flex lg:w-auto lg:items-center lg:space-x-6">
           <li>
-            <Link href="/about">
+            <Link href="/about" passHref>
               <a className="text-sm text-gray-400 hover:text-gray-500">About</a>
             </Link>
           </li>
@@ -59,7 +78,7 @@ export default function Navigation() {
             </svg>
           </li>
           <li>
-            <Link href="/what-we-do">
+            <Link href="/what-we-do" passHref>
               <a className="text-sm text-gray-400 hover:text-gray-500">
                 What We Do
               </a>
@@ -82,7 +101,7 @@ export default function Navigation() {
             </svg>
           </li>
           <li>
-            <Link href="/who-we-serve">
+            <Link href="/who-we-serve" passHref>
               <a className="text-sm text-gray-400 hover:text-gray-500">
                 Who We Serve
               </a>
@@ -105,7 +124,7 @@ export default function Navigation() {
             </svg>
           </li>
           <li>
-            <Link href="/our-events">
+            <Link href="/our-events" passHref>
               <a className="text-sm text-gray-400 hover:text-gray-500">
                 Events
               </a>
@@ -128,25 +147,32 @@ export default function Navigation() {
             </svg>
           </li>
           <li>
-            <Link href="/support-us">
+            <Link href="/support-us" passHref>
               <a className="text-sm font-bold text-green-600">Support Us</a>
             </Link>
           </li>
         </ul>
+        <a
+          className="custom-dbox-popup hidden rounded-l-xl rounded-t-xl bg-green-500 py-2 px-6 text-sm font-bold text-white transition duration-200 hover:bg-green-600 lg:inline-block"
+          href="https://donorbox.org/general-96"
+        >
+          Donate
+        </a>
       </nav>
       {isOpen && (
-        <div className="navbar-menu fixed top-0 left-0 bottom-0 z-50 w-5/6 max-w-sm">
-          <div className="navbar-backdrop fixed inset-0 bg-gray-800 opacity-25"></div>
+        <div className="fixed top-0 left-0 bottom-0 z-50 w-5/6 max-w-sm">
+          <div className="fixed inset-0 bg-gray-800 opacity-25"></div>
           <nav className="relative flex h-full w-full flex-col overflow-y-auto border-r bg-white py-6 px-6">
             <div className="mb-8 flex items-center">
-              <a className="mr-auto text-3xl font-bold leading-none" href="#">
-                {/* <Image
+              <Link href="/" passHref>
+                <a className="mr-auto text-3xl font-bold leading-none">
+                  <Image
                   className="h-16 w-16"
                   src={logo}
                   alt=""
-                  width="100"
-                  height="100"
-                /> */}
+                    width="80"
+                    height="80"
+                  />
               </a>
               <button onClick={handleToggle} className="navbar-close">
                 <svg
@@ -208,14 +234,19 @@ export default function Navigation() {
               <div className="pt-6"></div>
 
               <div className="text-center">
-                <Link href="https://www.facebook.com/HerManyVoices/">
+                <Link href="https://www.facebook.com/HerManyVoices/" passHref>
                   <a className="inline-block px-1">
                     <FacebookSocialLogo alt="" className="text-2xl" />
                   </a>
                 </Link>
-                <Link href="https://twitter.com/HerManyVoices">
+                <Link href="https://twitter.com/HerManyVoices" passHref>
                   <a className="inline-block px-1">
                     <TwitterSocialLogo alt="" className="text-2xl" />
+                  </a>
+                </Link>
+                <Link href="https://instagram.com" passHref>
+                  <a className="inline-block px-1">
+                    <InstagramSocialLogo alt="" className="text-2xl" />
                   </a>
                 </Link>
               </div>
