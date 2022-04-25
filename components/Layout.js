a247e75a15445634e127d49fe43d59dc19a7539a
mandihamza <mandihamza@gmail.com>
2022-04-25 14:10:00 -0600

    remove event banner

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,3 @@
-import Banner from './Banner'
 import Navigation from './Navigation'
 import NewsletterCTA from '@components/NewsletterCTA'
 import Footer from './Footer'
@@ -6,7 +5,6 @@ import Footer from './Footer'
 export default function Layout({ children }) {
   return (
     <>
-      <Banner />
       <Navigation />
       <main>{children}</main>
       <NewsletterCTA />
