f40b1f5d10d33a76a4e5eeef8e05e5d3fd4f1c9f
Mandihamza <mandihamza@gmail.com>
2022-06-24 16:21:01 -0600

    fix formatting

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -11,26 +11,26 @@ export default function ProjectCard({
   return (
     <div id={id} className="mb-6 w-full px-3 lg:w-1/4 flex">
       <div className="overflow-hidden rounded shadow w-full">
-        {image? (
+        {image ? (
           <img
-          className="rounded-t h-64 w-full object-cover"
-          src={image ? image : null}
-          alt={altText}
-        ></img>
+            className="rounded-t h-64 w-full object-cover"
+            src={image ? image : null}
+            alt={altText}
+          ></img>
         ) : (
           <div className="rounded-t h-64 w-full bg-gradient-to-r from-[#99f6e4] to-[#d9f99d]"></div>
         )}
-        
+
         <div className="rounded-b bg-white p-6 h-full flex flex-col">
           <h2 className="my-2 text-1xl font-bold">{title}</h2>
           <p>{shortDescription}</p>
           <ProjectModal
-          id={id}
-          title={title}
-          description={description}
-          image={image}
-          altText={altText}
-           />
+            id={id}
+            title={title}
+            description={description}
+            image={image}
+            altText={altText}
+          />
         </div>
       </div>
     </div>
