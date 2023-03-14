80145ec22c406c8f1e39a2bc1745a05c2340e82c
Mandihamza <mandihamza@gmail.com>
2023-03-14 11:11:49 -0600

    Delete commented code

--- public/assets/icons/SpeakerIcon.js
+++ public/assets/icons/SpeakerIcon.js
@@ -5,8 +5,3 @@ export default function SpeakerIcon() {
         </svg>
     )
 }
-
-
-{/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-  <path stroke-linecap="round" stroke-linejoin="round"  />
-</svg> */}
